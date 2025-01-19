2cd595fd2769b6bef7fe7c0598fe6f3af93947ff
bmountain <tomo.aoyama.jp@gmail.com>
2025-01-19 20:49:41 +0900

    tkinterにキーボードイベントを追加

--- screenshot_test.py
+++ screenshot_test.py
@@ -3,37 +3,78 @@ import screeninfo
 
 from PIL import ImageGrab
 from datetime import datetime
+import threading
+from pynput import keyboard
 
 
-class MyCanvasApp:
+class KeyboardListener(keyboard.Listener):
+    """テンキーを取得してtkinterにイベントを送信"""
+
     def __init__(self, root):
+        super().__init__(on_press=self.on_press)
+        self.daemon = True
+        self.stop_event = threading.Event()
+        self.mouse_screenshot = threading.Event()
         self.root = root
 
-        # Set Variables
+    def on_press(self, key):
+        if hasattr(key, "vk") and 96 <= key.vk <= 105:
+            if key.vk == 97:
+                self.root.event_generate("<<MouseScreenshotEvent>>")
+            if key.vk == 96:
+                self.root.event_generate("<<FullScreenshotEvent>>")
+            if key.vk == 105:
+                self.root.event_generate("<<ExitEvent>>")
+            if key.vk == 100:
+                self.root.event_generate("<<BackEvent>>")
+            if key.vk == 102:
+                self.root.event_generate("<<ForwardEvent>>")
+
+
+class MouseScreenshot:
+    def __init__(self, root):
+        self.root = root
         self.rect_id = None
         self.topx, self.topy, self.botx, self.boty = 0, 0, 0, 0
-        # Get the current screen width and height
+        self.set_root_appearance()
+        self.set_canvas_appearance()
+        self.set_bind()
+        self.make_app_invisible()
+        keyboard_listener = KeyboardListener(self.root)
+        keyboard_listener.start()
+
+    def set_bind(self):
+        """イベントに関数をバインド"""
+        # マウスイベント
+        self.canvas.bind("<Button-1>", self.get_mouse_posn)
+        self.canvas.bind("<B1-Motion>", self.update_sel_rect)
+        self.canvas.bind("<ButtonRelease-1>", self.get_mouse_screenshot)
+        self.canvas.bind("<Button-2>", self.make_app_invisible)
+        # キーボードイベント（KeyboardListenerから取得）
+        self.root.bind("<<MouseScreenshotEvent>>", self.on_mouse_screenshot_event)
+        self.root.bind("<<FullScreenshotEvent>>", self.on_full_screenshot_event)
+        self.root.bind("<<BackEvent>>", self.on_back_event)
+        self.root.bind("<<ForwardEvent>>", self.on_forward_event)
+        self.root.bind("<<ExitEvent>>", self.on_exit_event)
+
+    def set_root_appearance(self):
+        """外観の設定"""
         screen = screeninfo.get_monitors()[0]
         self.screen_width = screen.width
         self.screen_height = screen.height
-        # Create root window
-        root_geometry = (
-            str(self.screen_width) + "x" + str(self.screen_height)
-        )  # Creates a geometric string argument
-        root.geometry(root_geometry)  # Sets the geometry string value
+        self.root_geometry = str(self.screen_width) + "x" + str(self.screen_height)
+        self.root.geometry(self.root_geometry)
+        self.root.overrideredirect(True)
+        self.root.wait_visibility(self.root)
+        self.root.attributes("-alpha", 0.25)
 
-        root.overrideredirect(True)
-        root.wait_visibility(root)
-        root.attributes("-alpha", 0.25)  # Set windows transparent
-
-        # Create canvas on root windows
+    def set_canvas_appearance(self):
+        """マウスキャプチャモード時の外観設定"""
         self.canvas = tk.Canvas(
-            root, width=self.screen_width, height=self.screen_height
-        )  # Create canvas
-        self.canvas.config(cursor="cross")  # Change mouse pointer to cross
+            self.root, width=self.screen_width, height=self.screen_height
+        )
+        self.canvas.config(cursor="cross")
         self.canvas.pack()
-
-        # Create selection rectangle (invisible since corner points are equal).
         self.rect_id = self.canvas.create_rectangle(
             self.topx,
             self.topy,
@@ -44,54 +85,47 @@ class MyCanvasApp:
             outline="",
         )
 
-        self.canvas.bind(
-            "<Button-1>", self.get_mouse_posn
-        )  # Left click gets mouse position
-        self.canvas.bind(
-            "<B1-Motion>", self.update_sel_rect
-        )  # Mouse drag updates selection area
-        self.canvas.bind(
-            "<ButtonRelease-1>", self.get_screenshot
-        )  # Right click gets screenshıt, no selection will result full
-        self.canvas.bind(
-            "<Button-2>", lambda x: root.destroy()
-        )  # Quit without screenshot with middle click
+    def make_app_invisible(self, event=None):
+        self.root.withdraw()
 
-    # Get mouse position function
+    def on_mouse_screenshot_event(self, event):
+        self.root.deiconify()
+
+    def on_full_screenshot_event(self, event):
+        filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.png")
+        img = ImageGrab.grab()
+        img.save(filename)
+
+    def on_back_event(self, event):
+        print("back event!")
+
+    def on_forward_event(self, event):
+        print("forward event!")
+
+    def on_exit_event(self, event):
+        print("Exit")
+        self.root.destroy()
+
     def get_mouse_posn(self, event):
         self.topx, self.topy = event.x, event.y
 
-    # Update selection rectangle function
     def update_sel_rect(self, event):
         self.botx, self.boty = event.x, event.y
-        self.canvas.coords(
-            self.rect_id, self.topx, self.topy, self.botx, self.boty
-        )  # Update selection rect.
+        self.canvas.coords(self.rect_id, self.topx, self.topy, self.botx, self.boty)
 
-    # Get screenshot function
-    def get_screenshot(self, event):
+    def get_mouse_screenshot(self, event):
+        self.topx, self.botx = min(self.topx, self.botx), max(self.topx, self.botx)
+        self.topy, self.boty = min(self.topy, self.boty), max(self.topy, self.boty)
+        self.root.withdraw()
 
-        if self.topx > self.botx:
-            self.topx, self.botx = self.botx, self.topx
-
-        if self.topy > self.boty:
-            self.topy, self.boty = self.boty, self.topy
-        # self.root.after(
-        #     15
-        # )  ##### Wait for tkinter destruction, increase if you see a tint in your screenshots
-        self.root.destroy()  # Destroy tkinter, otherwise a transparent window will be on top of desktop
-        filename = datetime.now().strftime(
-            "%Y-%m-%d_%H-%M-%S.png"
-        )  # filename determine
-        img = ImageGrab.grab(
-            bbox=(self.topx, self.topy, self.botx, self.boty)
-        )  # Actual screenshot
-        img.save(filename)  # Screenshot save to file
+        filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S.png")
+        img = ImageGrab.grab(bbox=(self.topx, self.topy, self.botx, self.boty))
+        img.save(filename)
 
 
 def main():
     root = tk.Tk()
-    app = MyCanvasApp(root)
+    app = MouseScreenshot(root)
     app.root.mainloop()
 
 
--- threading_test.py
+++ threading_test.py
@@ -1,62 +1,66 @@
-import keyboard  
+import keyboard
 import threading
 import tkinter as tk
 from pynput import keyboard, mouse
-from screenshot_test import main, MyCanvasApp
+
 
 class KeyboardLister(keyboard.Listener):
     """テンキーを取得する"""
+
     def __init__(self):
         super().__init__(on_press=self.on_press)
         self.daemon = True
         self.stop_event = threading.Event()
         self.mouse_screenshot = threading.Event()
+
     def on_press(self, key):
-        if hasattr(key, 'vk') and 96 <= key.vk <= 105:
-            print(f'{key} pressed!')
+        if hasattr(key, "vk") and 96 <= key.vk <= 105:
+            print(f"{key} pressed!")
             if key.vk == 97:
-                print('1 pressed!')
+                print("1 pressed!")
                 self.mouse_screenshot.set()
             if key.vk == 105:
-                print('9 pressed! Sending stop request!')
+                print("9 pressed! Sending stop request!")
                 self.send_stop_signal()
 
     def send_stop_signal(self):
         self.stop_event.set()
-                
+
 
 class MouseListener(mouse.Listener):
     """マウスを取得する"""
+
     def __init__(self):
         super().__init__(on_move=self.on_move, on_click=self.on_click)
         self.daemon = True
-    def on_move(self, x, y):
-        print('Pointer moved to {0}'.format(
-            (x, y)))
-    def on_click(self, x, y, button, pressed):
-        print('{0} {1} at {2}'.format(
-            button,
-            'Pressed' if pressed else 'Released',
-            (x, y)))
 
-class App():
+    def on_move(self, x, y):
+        print("Pointer moved to {0}".format((x, y)))
+
+    def on_click(self, x, y, button, pressed):
+        print(
+            "{0} {1} at {2}".format(
+                button, "Pressed" if pressed else "Released", (x, y)
+            )
+        )
+
+
+class App:
     def __init__(self):
         pass
+
     def run(self):
         keyboard_listener = KeyboardLister()
         mouse_listener = MouseListener()
         keyboard_listener.start()
         mouse_listener.start()
         while True:
-            if keyboard_listener.mouse_screenshot.is_set(): 
-                print('would take mouse screenshot')
-                main()
+            if keyboard_listener.mouse_screenshot.is_set():
+                print("would take mouse screenshot")
             if keyboard_listener.stop_event.is_set():
                 break
         keyboard_listener.stop_event.wait()
 
 
-    
 if __name__ == "__main__":
-    app = App()
-    app.run()
+    pass
